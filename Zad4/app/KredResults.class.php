839371cfdaef3a527b9632f2cf9e0167ea3f4262
Pawel <lipkap1@gmail.com>
2024-11-28 19:05:55 +0100

    deleted unnecessary files

--- Zad4/app/KredResults.class.php
+++
@@ -1,7 +0,0 @@
-<?php
-class KredResults{
-
-    public $monthly;
-    public $total;
-}
-?>
--- Zad4/app/kred_contr.php
+++
@@ -1,6 +0,0 @@
-<?php 
-require_once dirname(__FILE__).'/../config.php';
-require_once $config->root_path.'/app/KredContr.class.php';
-
-$ctrl = new KredContr();
-$ctrl->calculate();
